b3fc439d0241305a958d202efd3bb106c2132288
Maxim Karachyov <karachyov.m@gmail.com>
2023-02-02 11:01:20 +0300

    brozex.js

--- build/js/brozex.js
+++ build/js/brozex.js
@@ -806,19 +806,34 @@ void 0!==c?null===c?void r.removeAttr(a,b):e&&"set"in e&&void 0!==(d=e.set(a,c,b
 });
 
 
+if(document.querySelector('.swiper-main')) {
+    const swiperMain = new Swiper('.swiper-main', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    })
+}
 
-const swiperMain = new Swiper('.swiper-main', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-})
+if(document.querySelector('.swiper-brands')) {
+    const swiperBrands = new Swiper('.swiper-brands', {
+        slidesPerView: 7.8,
+        spaceBetween: 53,
+    })
+}
 
-const swiperBrands = new Swiper('.swiper-brands', {
-    slidesPerView: 7.8,
-    spaceBetween: 53,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-})
+if(document.querySelector('.swiper-products')) {
+    document.querySelectorAll('.slider-products').forEach(n => {
+        const swiperProducts = new Swiper(n.querySelector('.swiper-products'), {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            observer: true,
+            observeParents: true,
+            navigation: {
+                nextEl: n.querySelector('.swiper-button-next'),
+                prevEl: n.querySelector('.swiper-button-prev'),
+            },
+        });
+    });
+}
--- src/js/brozex.js
+++ src/js/brozex.js
@@ -3,19 +3,34 @@
 @@include('./partials/jquery.formstyler.min.js')
 @@include('./partials/remodal.js')
 
+if(document.querySelector('.swiper-main')) {
+    const swiperMain = new Swiper('.swiper-main', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    })
+}
 
-const swiperMain = new Swiper('.swiper-main', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-})
+if(document.querySelector('.swiper-brands')) {
+    const swiperBrands = new Swiper('.swiper-brands', {
+        slidesPerView: 7.8,
+        spaceBetween: 53,
+    })
+}
 
-const swiperBrands = new Swiper('.swiper-brands', {
-    slidesPerView: 7.8,
-    spaceBetween: 53,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-})
+if(document.querySelector('.swiper-products')) {
+    document.querySelectorAll('.slider-products').forEach(n => {
+        const swiperProducts = new Swiper(n.querySelector('.swiper-products'), {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            observer: true,
+            observeParents: true,
+            navigation: {
+                nextEl: n.querySelector('.swiper-button-next'),
+                prevEl: n.querySelector('.swiper-button-prev'),
+            },
+        });
+    });
+}
